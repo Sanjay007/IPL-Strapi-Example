db56161ec51241df5248d78aa57c4f0a8eb004ac
4Bits Solutions <57039494+4Bits-solutions@users.noreply.github.com>
2023-01-14 12:30:51 +0530

    api updated

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+ module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'dpg-cf14p8p4reb5o40saadg-a.singapore-postgres.render.com'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'ipl'),
+          user: env('DATABASE_USERNAME', 'iplp01'),
+          password: env('DATABASE_PASSWORD', 'KKMlH4s5QtioVdbYUubDR9xHPXY3Q1x8'),
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
+    });
